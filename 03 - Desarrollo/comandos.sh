831c1d99f78b5cfc6cb4a379d33f3c31a93c6874
ssgalo <santigalo32@gmail.com>
2025-09-19 22:46:04 -0300

    update comandos

--- 03 - Desarrollo/comandos.sh
+++ 03 - Desarrollo/comandos.sh
@@ -1,6 +1,3 @@
-# Crear el proyecto React con Vite
-npm create vite@latest frontend/analizador-gastos-front
-
 # Navegar al directorio del proyecto
 cd frontend/analizador-gastos-front
 
@@ -9,6 +6,3 @@ npm install
 
 # Ejecutar el servidor de desarrollo
 npm run dev
-
-# TAILWIND
-npm install -D tailwindcss @tailwindcss/vite
